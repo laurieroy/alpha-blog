f7705b525be09bc5b44620d849b6c1f2bee512a7
lauretteroy <laurieroy.dev@gmail.com>
2020-12-29 15:22:53 -0700

    Add categories index list and tests

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -20,10 +20,10 @@ class  CategoryTest < ActiveSupport::TestCase
 	end
 
 	test "name should not be too long" do
-		@category.name = "a" * 21
+		@category.name = "a" * 31
 		assert_not @category.valid?
 	end
-	
+
 	test "name should not be too short" do
 		@category.name = "a" * 2
 		assert_not @category.valid?
